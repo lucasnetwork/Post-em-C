5461a08a18f55460cdf5f4a00092ce56ac1dc853
Lucas Ribeiro <lucasribeiro61374@gmail.com>
2019-11-05 00:03:47 -0300

    Adicionando tela de login.

--- main.c
+++ main.c
@@ -2,16 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-int Login(int password){
-
-}
-
-int signUp(){
-
-}
-
 int main(){
-    char email[6] = "lucas";
-    getPost(email);
+    char email[6];
+    int password,Login;
+    printf("Ja possui login?\nSim(0)\nNao(1)");
+    scanf("%i",&Login);
+    switch (Login){
+        case 0:
+            printf("Digite o email:\n");
+            scanf("%s",&email);
+            printf("Digite a senha:\n");
+            scanf("%i",&password);
+            system("cls");
+            if(login(password,email)==0){
+                printf("passou");
+            }else{
+                printf("Email ou senha incorretos");
+            }
+            break;
+        case 1:
+            printf("Digite o email");
+    }
     return 0;
 }
--- validation.c
+++ validation.c
@@ -1,9 +1,28 @@
 #include <string.h>
 
+int login(int password,char* email){
+    int user;
+    for(user=0; user <= 10;user++){
+        if(password == 123456 && strcmp(email,"lucas")==0){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+}
 int postUserExists(char* email){
     if(strcmp(email,"lucas")==0){
         return "existe";
     }else{
         return "nao existe";
     }
+}
+
+
+int userExist(char* email){
+
+}
+
+int signUp(int password,char* email){
+    userExist(email);
 }
