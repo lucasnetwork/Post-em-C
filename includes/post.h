7b6a7cca22b3266801f336cb3da11ce1a5607f30
Lucas Ribeiro <lucasribeiro61374@gmail.com>
2019-11-15 22:07:22 -0300

    adicionando funções de editar e deletar usuario.

--- includes/post.h
+++ includes/post.h
@@ -2,56 +2,122 @@ int createPost(char* email){
 	struct POST post;
 	int continuar,a;
 	a= 1;
+	strcat(post.email,email);
 	continuar = 0;
 	FILE *postFile;
-	postFile = fopen("posts.txt","a+");
 	while(continuar ==0){
+		postFile = fopen("posts.txt","a+");
+		a=1;
 		printf("Escreva seu post: ");
 		while(a== 1){
-			if(strlen(post.postContent)>10){
+			scanf("%s",&post.postContent);
+			if(strlen(post.postContent)>1000){
 				printf("post muito grande\n");
 				a= 1;
 			}else{
 				a=0;
 			}
 		}
+		post.id = checkPostId(email);
 		fwrite(&post,1,sizeof(post),postFile);	
-		printf("post cadastra com sucesso");
+		printf("post cadastrado com sucesso");
 		getch();
+		fclose(postFile);
 		system("cls");
 		printf("Deseja continuar?\n");
 		scanf("%i",&continuar);
 	}
-	fclose(postFile);
 	return 0;
 }
-
-
+int deletePost(char* email, int id){
+	struct POST post;
+	FILE *postFile;
+	FILE *postFileAux;
+	postFile = fopen("posts.txt","a+");
+	postFileAux = fopen("postsaux.txt","a+");
+	system("cls");
+	while(fread(&post,sizeof(post),1,postFile)){
+		if(strcmp(email,post.email)!=0){
+			fwrite(&post,sizeof(post),1,postFileAux);
+		}
+		if(strcmp(email,post.email)== 0 && id != post.id){
+			fwrite(&post,sizeof(post),1,postFileAux);
+		}
+	}
+	fclose(postFile);
+	fclose(postFileAux);
+	remove("posts.txt");
+	rename("postsaux.txt","posts.txt");
+	return 0;
+}
+int editPost(char* email,int id){
+	if(postUserExists(email,id)==0){
+		struct POST post;
+		struct POST postAux;
+		FILE *postFile;
+		FILE * postFileAux;
+		postFile = fopen("posts.txt","a+");
+		postFileAux = fopen("postsaux.txt","a+");
+		while(fread(&post,sizeof(post),1,postFile)!=0){
+			if(strcmp(email,post.email)== 0 && id==post.id){
+				printf("Digite o novo conteudo:\n");
+				scanf("%s",&post.postContent);
+				fwrite(&post,sizeof(post),1,postFileAux);
+			}else{
+				fwrite(&post,sizeof(post),1,postFileAux);
+			}
+		}
+		fclose(postFile);
+		fclose(postFileAux);
+		remove("posts.txt");
+		rename("postsaux.txt","posts.txt");
+	}else{
+		printf("post nao existe");
+		getch();
+	}
+}
 int getPostsUser(char* email){
+	int id,action;
 	struct POST post;
 	FILE *postFile;
 	postFile = fopen("posts.txt","a+");
+	printf("Posts do usuario %s\n",post.email);
 	while(fread(&post,sizeof(post),1,postFile)!=0){
-		printf("%i",strcmp(email,post.email));
             if(strcmp(email,post.email)== 0){
-				printf("email:\n%s\n",post.email);
-				printf("%s\n",post.postContent);
-				getch();
+				printf("\n  email:%s\n",post.email);
+				printf("  id:%i\n",post.id);
+				printf("  %s\n\n",post.postContent);
             }
-	}	
+	}
+	printf("\n Deseja fazer alguma ação?\nDeletar(0)\nEditar(1)\nSair(3)");
+	scanf("%i",&action);
 	fclose(postFile);
+	switch (action){
+	case 0:
+		printf(" Id do post:");
+		scanf(" %i",&id);
+		deletePost(email,id);
+		break;
+	case 1:
+		printf(" Id do post:");
+		scanf(" %i",&id);
+		editPost(email,id);
+		break;
+	default:
+		break;
+	}
 	return 0;
 }
 int getPosts(){
 	struct POST post;
 	FILE *postFile;
 	postFile = fopen("posts.txt","a+");
+	printf("           Timeline\n");
+	printf("-----------------------------\n");
 	while(fread(&post,sizeof(post),1,postFile)!=0){
-		printf("email:%s\n",post.email);
-		printf("%s\n\n",post.postContent);
+		printf("  %s@gmail.com:\n",post.email);
+		printf("     %s\n\n",post.postContent);
 	}	
-	getch();
 	fclose(postFile);
 	return 0;
 }
-
--- includes/validation.h
+++ includes/validation.h
@@ -1,9 +1,16 @@
-int postUserExists(char* email){
-    if(strcmp(email,"lucas")==0){
-        return 0;
-    }else{
-        return 1;
-    }
+int postUserExists(char* email,int id){
+    struct POST post;
+	FILE *postFile;
+	postFile = fopen("posts.txt","a+");
+	system("cls");
+	while(fread(&post,sizeof(post),1,postFile)){
+		if(strcmp(email,post.email)==0 && id == post.id){
+			fclose(postFile);
+            return 0;
+		}
+	}
+	fclose(postFile);
+	return 1;
 }
 
 int isNumber(int number){
@@ -19,8 +26,6 @@ int userExist(char* email, char* password){
     struct USER user;
     while(fread(&user,sizeof(user),1,usersFile)!=0){
         if(strcmp(password,"")==0){
-            printf("%i",strcmp(email,user.email));
-            getch();
             if(strcmp(email,user.email)== 0){
                 fclose(usersFile);
                 return 0;
@@ -33,5 +38,20 @@ int userExist(char* email, char* password){
         }
     }
     fclose(usersFile);
+    
     return 2;
+}
+int checkPostId(char* email){
+	int id;
+	id = 0;
+	struct POST post;
+	FILE *postFile;
+	postFile = fopen("posts.txt","a+");
+	while(fread(&post,sizeof(post),1,postFile)!=0){
+            if(strcmp(email,post.email)== 0 && id == post.id){
+                id++;
+            }
+	}	
+	fclose(postFile);
+	return id;
 }
