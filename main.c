597a2a481692f9f44b6ef747ba41bd85380b142e
Lucas Ribeiro <lucasribeiro61374@gmail.com>
2019-11-20 00:26:18 -0300

    Criando função login para diminuir função main.

--- main.c
+++ main.c
@@ -1,13 +1,58 @@
 #include "./includes/index.h"
-int main(){
-    setlocale(LC_ALL,"");
-    int password,Login,task;
+
+int login(){
+    int task;
+    task = 0;
     char emailLog[50];
 	char passwordLog[16];
+    printf(" Digite seu email:");
+    scanf("%s",&emailLog);
+    printf(" Digite sua senha:");
+    scanf("%s",&passwordLog);
+    system("cls");
+    if(getLogin(emailLog,passwordLog)==0){
+        while(task !=3){
+            system("cls");
+            printf(" Criar ou visualizar posts?\n\nCriar(0)\nVisualizar(1)\nEditar Usuario(2)\nDeletar usuario(3)\nExit(4)\n");
+            scanf("%i",&task);
+            switch (task){
+                case 0:
+                    system("cls");
+                    createPost(emailLog);
+                    break;
+                case 1:
+                    system("cls");
+                    getPostsUser(emailLog);
+                    break;
+                case 2:
+                    system("cls");
+                    editUser(emailLog);
+                    task = 3;
+                    break;
+                case 3:
+                    deleteUser(emailLog);
+                    task = 3;
+                    break;
+                case 4:
+                    system("cls");
+                    task = 3;
+                    break;
+            }
+                    
+        }
+    }else{
+        printf("%s",LOGIN_ERROR);
+        getch();
+    }
+}
+
+int main(){
+    setlocale(LC_ALL,"");
+    int password,Login;
+    
     //0 para login em email, e 1 para se registrar;
     for(;;){
         system("cls");
-        task = 0;
         getPosts();
         printf(" Ja possui login?\nSim(0)\nNao(1)\nexit(2)\n");
         while(!scanf("%i",&Login)){
@@ -20,45 +65,7 @@ int main(){
         //login of user
         switch (Login){
             case 0:
-                printf(" Digite seu email:");
-                scanf("%s",&emailLog);
-                printf(" Digite sua senha:");
-                scanf("%s",&passwordLog);
-                system("cls");
-                if(getLogin(emailLog,passwordLog)==0){
-                    while(task !=3){
-                        system("cls");
-                    printf(" Criar ou visualizar posts?\n\nCriar(0)\nVisualizar(1)\nEditar Usuario(2)\nDeletar usuario(3)\nExit(4)\n");
-                    scanf("%i",&task);
-                    switch (task){
-                        case 0:
-                            system("cls");
-                            createPost(emailLog);
-                            break;
-                        case 1:
-                            system("cls");
-                            getPostsUser(emailLog);
-                            break;
-                        case 2:
-                            system("cls");
-                            editUser(emailLog);
-                            task = 3;
-                            break;
-                        case 3:
-                            deleteUser(emailLog);
-                            task = 3;
-                            break;
-                        case 4:
-                            system("cls");
-                            task = 3;
-                            break;
-                    }
-                    
-                    }
-                }else{
-                    printf("%s",LOGIN_ERROR);
-                    getch();
-                }
+                login();
                 break;
             case 1:
                 getRegister();
